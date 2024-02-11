003469c2a6d39bf295c8e8962ea481f7de6f7554
Your Name <you@example.com>
2024-02-11 18:30:41 +0100

    Update webpack configuration and package.json***
    ***Update file paths in index.html and manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "",
   },
   devServer: {
     static: path.resolve(__dirname, "/dist"),
