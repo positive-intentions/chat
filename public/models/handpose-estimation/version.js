c98c6d693da9c8315c76538ad837399213c8aa90
Your Name <you@example.com>
2024-03-21 16:00:37 +0100

    recompiled

--- public/models/handpose-estimation/version.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/** @license See the LICENSE file. */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = void 0;
-// This code is auto-generated, do not modify this file!
-var version = '2.0.0';
-exports.version = version;
-//# sourceMappingURL=version.js.map
